5a6b4b856bbffc5d759d0262da11ef9617a9b846
Harsh <harsh1879.be20@chitkara.edu.in>
2022-08-26 14:50:53 +0530

    Question 1

---
+++ q1.cpp
@@ -0,0 +1,66 @@
+/* Harsh Kumar Sharma
+ Roll No:2010992879
+ Set-05
+ Q-1
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int lenoflongsubarr(int nums[],int n,int x)
+{
+    unordered_map<int, int> sub;
+    int sum = 0, max = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        sum += nums[i];
+
+        if (sum == x)
+            max = i + 1;
+
+        if (sub.find(sum) == sub.end())
+            sub[sum] = i;
+
+        if (sub.find(sum - x) != sub.end())
+        {  
+
+            if (max < (i - sub[sum - x]))
+                max = i - sub[sum - x];
+        }
+    }
+
+    return max;
+}
+    
+
+
+int main() {
+    int nums[]={5,6,-5,5,3,5,3,-2,0};
+    int n = sizeof(nums) / sizeof(nums[0]);
+    int x = 8;
+    int temp;
+
+    try
+    {
+        temp = lenoflongsubarr(nums, n, x);
+        if (!temp)
+            throw("No subarray found");
+    }
+    catch (int error)
+    {
+        cout << "Error: " << error << endl;
+    }
+
+    cout << "Length = " << temp;
+    
+    
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
